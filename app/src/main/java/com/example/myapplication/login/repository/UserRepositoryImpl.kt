58752c7005b44c8fe63c46e521b093e6f18265b8
daniel_redondo_assis <dtz.assis@gmail.com>
2025-07-03 01:03:00 -0300

    - Added Some Screenshots to The Project

--- app/src/main/java/com/example/myapplication/login/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/myapplication/login/repository/UserRepositoryImpl.kt
@@ -5,6 +5,6 @@ class UserRepositoryImpl : UserRepository {
     override fun loginUser(email: String, password: String) : Boolean {
         // api call using retrofit + okttph
         // here you can mock an successful or error result
-        return true
+        return false
     }
 }
